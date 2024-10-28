20e53856f8153834323d47b7990eff678118dee8
vhandeantgamers <vhandeantgamers@gmail.com>
2024-10-28 14:43:21 +0000

    menambahkan firebase CRUD

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/app/modules/home/views/profile_view.dart';
 import 'package:myapp/app/page/home_page.dart'; // Pastikan untuk mengimpor halaman Medic
+import 'package:myapp/app/page/appointment_page.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -132,6 +133,12 @@ class HomeView extends StatelessWidget {
           } else if (index == 4) {
             // Jika Profile ditekan
             Get.to(ProfileView());
+          } else if (index == 2) {
+            // Jika Profile ditekan
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AppointmentPage()),
+            );
           }
         },
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapp/app/modules/home/views/profile_view.dart';
 import 'package:myapp/app/modules/home/views/home_view.dart'; // Memanggil HomeView
-import 'package:myapp/app/page/home_page.dart'; // Memanggil HomePage
+import 'package:myapp/app/page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+options: DefaultFirebaseOptions.currentPlatform,
+);
+runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
